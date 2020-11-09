39da0c2828e5a778321e184cc7c8298c0295ceaa
cf19carlosreina <71392158+cf19carlosreina@users.noreply.github.com>
2020-11-09 20:07:45 +0100

    Add files via upload

---
+++ M14UF1E01.py
@@ -0,0 +1,6 @@
+﻿print("2") #Mostrarà el que hi hagi entre cometes
+print(2) #Mostrarà el numero
+print(0x123) #Mostrarà el número octal 123 en decimal
+print(0o123) #Mostrarà el número hexadecimal 123 en decimal
+print ( "M'agrada \"Monty Python\"") #Mostrarà el que hi ha a la cadena i Monty Python entre cometes dobles
+print('Me gusta "Monty Python"') #Mostrarà el que hi ha a la cadena i Monty Python entre cometes dobles
