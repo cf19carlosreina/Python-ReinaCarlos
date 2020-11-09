e6638f844f19b8ea24c92d432505abee693b1dc3
cf19carlosreina <71392158+cf19carlosreina@users.noreply.github.com>
2020-11-09 19:51:09 +0100

    Add files via upload

---
+++ M14UF1E02.py
@@ -0,0 +1,4 @@
+﻿print("I'm Monty Python.") # Amb la següent funció es mostrarà per pantalla el que està dintre de les cadenes
+print("I'm \nMonty Python.") # Amb la següent funció es mostrarà per pantall el que està dintre de les cadenes, però farà un salt de pàgina després de la barra invertida i la "n".
+print("\'I'm Monty Python.\'") # Amb la següent funció es mostrarà tota la frase entre cometes simples
+print("\"I'm Monty Python.\"") # Amb la següent funció es mostrarà tota la frase entre cometes dobles
---
+++ M14UF1E03.py
@@ -0,0 +1,2 @@
+﻿major_dedat=input("Ets major d'edat? (True/False)")
+print("Ets major d'edat:", major_dedat)
