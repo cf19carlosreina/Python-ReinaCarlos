76643bb1e42217bebebe968593b6c51a9786b416
cf19carlosreina <71392158+cf19carlosreina@users.noreply.github.com>
2020-11-25 18:52:43 +0100

    Add files via upload

---
+++ Num_Flotant.py
@@ -0,0 +1,8 @@
+﻿flotant = float(input("Introdueix ún número flotant: "))
+flotant2 = float(input("Introdueix ún altre número flotant: "))
+if flotant>flotant2:
+    print(flotant, "és més gran que", flotant2)
+elif flotant==flotant2:
+    print(flotant, "és igual que", flotant2)
+else:
+    print(flotant, "és més petit que", flotant2)
