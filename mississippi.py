ffaa1c26024880a4e160b1571791e3183443ff99
cf19carlosreina <71392158+cf19carlosreina@users.noreply.github.com>
2020-12-09 18:45:10 +0100

    Add files via upload

---
+++ mississippi.py
@@ -0,0 +1,11 @@
+﻿import time
+
+    # Escribe un ciclo for que cuente hasta cinco.
+    # Cuerpo del ciclo: imprime el número de iteración del ciclo y la palabra "Mississippi".
+    # Cuerpo del ciclo - uso: time.sleep (1)
+
+# Escribe una función de impresión con el mensaje final.
+for cont in range(1, 6):
+    print(cont,"Mississippi")
+    time.sleep (1)
+print("¡Listo o no, ahí voy!")
