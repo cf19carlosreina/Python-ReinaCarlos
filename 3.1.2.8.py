3ac492cbf919508472bbc0fe96c117fc20968015
cf19carlosreina <71392158+cf19carlosreina@users.noreply.github.com>
2021-02-03 18:31:59 +0100

    Update and rename 3.1.2.8.py to 3.1.2.9.py

--- 3.1.2.8.py
+++
@@ -1,4 +0,0 @@
-palabra=input("Ingresa una palabra: ")
-while True:
-    palabra=input("Ingresa otra palabra: ")
-    break
---
+++ 3.1.2.9.py
@@ -0,0 +1,5 @@
+while True:
+    palabra = str(input("Ingresa una palabra: "))
+    if palabra == "chupacabra":
+        break
+print("¡Has dejado el ciclo con éxito!")
