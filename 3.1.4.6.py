533ea1a99b81456b7854aa8229379cdf63f3392e
cf19carlosreina <71392158+cf19carlosreina@users.noreply.github.com>
2021-02-28 23:00:11 +0100

    Add files via upload

---
+++ 3.1.4.6.py
@@ -0,0 +1,10 @@
+﻿listaSombrero = [1, 2, 3, 4, 5] # Esta es una lista existente de números ocultos en el sombrero.
+
+# Paso 1: escribe una línea de código que solicite al usuario
+# para reemplazar el número de en medio con un número entero ingresado por el usuario.
+listaSombrero[0] = int(input("Escribe un número: " ))
+# Paso 2: escribe aquí una línea de código que elimine el último elemento de la lista.
+del listaSombrero[4]
+# Paso 3: escribe aquí una línea de código que imprima la longitud de la lista existente.
+print("Longitud de la lista:", len(listaSombrero))
+print(listaSombrero)
